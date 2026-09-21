bfdf5dc32d5887608f235e67d4e3eff831c3f93d
Jose Navarro Esimi <josenkoho@gmail.com>
2026-09-21 21:54:57 +0500

    second commit - add print

--- .venv/program_example.py
+++ .venv/program_example.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('Jose Navarro')
     print('ИВТ - ИСУ- 203Б')
+    print('Y cambio el código todos los días')
 
 if __name__ == '__main__':
     main()
